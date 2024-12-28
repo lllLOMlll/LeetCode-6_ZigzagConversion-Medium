065629f225bef3e17a5ce77545d12b2777c03381
Louis-Olivier Major <louis.olivier.major@gmail.com>
2024-12-28 14:31:46 -0500

    Tests written

--- Program.cs
+++ Program.cs
@@ -2,4 +2,21 @@
     public string Convert(string s, int numRows) {
         return null;
     }
+
+static void Main(string[] args)
+{
+    Solution s = new Solution();
+
+    string string1 = "PAYPALISHIRING";
+    int numRows1 = 3;
+    Console.WriteLine("PAHNAPLSIIGYIR = " + s.Convert(string1, numRows1));
+
+    string string2 = "PAYPALISHIRING";
+    int numRows2 = 4;
+    Console.WriteLine("PINALSIGYAHRPI = " + s.Convert(string2, numRows2));
+
+    string string3 = "A";
+    int numRows3 = 1;
+    Console.WriteLine("A = " + s.Convert(string3, numRows3));
+}
 }
