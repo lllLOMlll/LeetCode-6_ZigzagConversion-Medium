dd2a8aaa9624963b0df6532032452429389ba813
Louis-Olivier Major <louis.olivier.major@gmail.com>
2025-01-03 21:57:03 -0500

    Code completed

--- Program.cs
+++ Program.cs
@@ -1,12 +1,42 @@
-﻿public class Solution {
+﻿using System.Runtime.InteropServices;
+using System.Text;
+
+public class Solution {
     public string Convert(string s, int numRows) {
         if (numRows == 1 || numRows >= s.Length)
         {
             return s;
         }
 
+        List<StringBuilder> rows = new List<StringBuilder>();
+        for (int i = 0; i < numRows; i++)
+        {
+            rows.Add(new StringBuilder());
+        }
 
-        return null;
+        int currentRow = 0;
+        bool goingDown = false;
+
+        foreach (char c in s)
+        {
+            rows[currentRow].Append(c);
+
+            if (currentRow == 0 || currentRow == numRows - 1)
+            {
+                goingDown = !goingDown;
+            }
+
+            currentRow += goingDown ? 1 : -1;
+        }
+
+        StringBuilder result = new StringBuilder();
+        foreach (StringBuilder row in rows)
+        {
+            result.Append(row);
+        }
+
+        return result.ToString();
+
     }
 
 static void Main(string[] args)
